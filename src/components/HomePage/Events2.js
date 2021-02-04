db15d60fd0b9d428195e635208614ecb118dc3ab
megha <meghagupta190801@gmail.com>
2021-02-04 17:31:13 +0530

    Update Events2.js

--- src/components/HomePage/Events2.js
+++ src/components/HomePage/Events2.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
 
 function Events2() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Carousel>
+        <Carousel.Item className="Events-item">
+          <img
+            className="d-block w-100"
+            src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item className="Events-item">
+          <img
+            className="d-block w-100"
+            src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item className="Events-item">
+          <img
+            className="d-block w-100"
+            src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 }
 
-export default Events2
+export default Events2;
