ad0ffecbde42e2eb1be577901c979227920c3e95
megha <meghagupta190801@gmail.com>
2021-02-08 21:56:04 +0530

    Update Events2.js

--- src/components/HomePage/Events2.js
+++ src/components/HomePage/Events2.js
@@ -1,30 +1,81 @@
 import React from "react";
-import Carousel from "react-bootstrap/Carousel";
+import { Carousel, Container, Row, Col } from "react-bootstrap";
 
 function Events2() {
   return (
     <div>
-      <Carousel>
-        <Carousel.Item className="Events-item">
-          <img
-            className="d-block w-100"
-            src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            alt="First slide"
-          />
+      <Carousel pause="false">
+        <Carousel.Item className="Events-item2" interval={1000}>
+          <Container>
+            <Row>
+              <Col>
+              <img
+                className="m-auto mx-auto"
+                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                alt="First slide"
+              /></Col>
+              <Col>
+              <img
+                className="m-auto mx-auto"
+                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                alt="First slide"
+              /></Col>
+              <Col>
+              <img
+                className="m-auto mx-auto"
+                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                alt="First slide"
+              /></Col>
+            </Row>
+          </Container>
         </Carousel.Item>
-        <Carousel.Item className="Events-item">
-          <img
-            className="d-block w-100"
-            src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            alt="Third slide"
-          />
+        <Carousel.Item className="Events-item2" interval={500}>
+        <Container>
+            <Row>
+              <Col>
+              <img
+                className="m-auto mx-auto"
+                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                alt="First slide"
+              /></Col>
+              <Col>
+              <img
+                className="m-auto mx-auto"
+                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                alt="First slide"
+              /></Col>
+              <Col>
+              <img
+                className="m-auto mx-auto"
+                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                alt="First slide"
+              /></Col>
+            </Row>
+          </Container>
         </Carousel.Item>
-        <Carousel.Item className="Events-item">
-          <img
-            className="d-block w-100"
-            src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            alt="Third slide"
-          />
+        <Carousel.Item className="Events-item2" interval={500}>
+        <Container>
+            <Row>
+              <Col>
+              <img
+                className="m-auto mx-auto"
+                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                alt="First slide"
+              /></Col>
+              <Col>
+              <img
+                className="m-auto mx-auto"
+                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                alt="First slide"
+              /></Col>
+              <Col>
+              <img
+                className="m-auto mx-auto"
+                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                alt="First slide"
+              /></Col>
+            </Row>
+          </Container>
         </Carousel.Item>
       </Carousel>
     </div>
