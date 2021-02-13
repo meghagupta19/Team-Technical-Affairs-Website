9b2e7073984b04346bc50380854ecd5b3e2ce0bb
megha <meghagupta190801@gmail.com>
2021-02-13 19:53:40 +0530

    Update Events2.js

--- src/components/HomePage/Events2.js
+++ src/components/HomePage/Events2.js
@@ -1,81 +1,66 @@
 import React from "react";
-import { Carousel, Container, Row, Col } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 
 function Events2() {
   return (
     <div>
-      <Carousel pause="false">
-        <Carousel.Item className="Events-item2" interval={1000}>
-          <Container>
-            <Row>
-              <Col>
-              <img
-                className="m-auto mx-auto"
-                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="First slide"
-              /></Col>
-              <Col>
-              <img
-                className="m-auto mx-auto"
-                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="First slide"
-              /></Col>
-              <Col>
-              <img
-                className="m-auto mx-auto"
-                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="First slide"
-              /></Col>
-            </Row>
-          </Container>
+      <Carousel pause="false" className="Events-content2">
+        <Carousel.Item className="Events-item2" interval={3000}>
+          <div className="Events-item2-img">
+            <img
+              className="m-auto mx-auto"
+              src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              alt="First slide"
+            />
+            <img
+              className="m-auto mx-auto img2"
+              src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              alt="First slide"
+            />
+            <img
+              className="m-auto mx-auto"
+              src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              alt="First slide"
+            />
+          </div>
         </Carousel.Item>
-        <Carousel.Item className="Events-item2" interval={500}>
-        <Container>
-            <Row>
-              <Col>
-              <img
-                className="m-auto mx-auto"
-                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="First slide"
-              /></Col>
-              <Col>
-              <img
-                className="m-auto mx-auto"
-                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="First slide"
-              /></Col>
-              <Col>
-              <img
-                className="m-auto mx-auto"
-                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="First slide"
-              /></Col>
-            </Row>
-          </Container>
+        <Carousel.Item className="Events-item2" interval={3000}>
+          <div className="Events-item2-img">
+            <img
+              className="m-auto mx-auto"
+              src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              alt="First slide"
+            />
+            <img
+              className="m-auto mx-auto img2"
+              src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              alt="First slide"
+            />
+            <img
+              className="m-auto mx-auto"
+              src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              alt="First slide"
+            />
+          </div>
         </Carousel.Item>
-        <Carousel.Item className="Events-item2" interval={500}>
-        <Container>
-            <Row>
-              <Col>
-              <img
-                className="m-auto mx-auto"
-                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="First slide"
-              /></Col>
-              <Col>
-              <img
-                className="m-auto mx-auto"
-                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="First slide"
-              /></Col>
-              <Col>
-              <img
-                className="m-auto mx-auto"
-                src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="First slide"
-              /></Col>
-            </Row>
-          </Container>
+        <Carousel.Item className="Events-item2" interval={3000}>
+          <div className="Events-item2-img">
+            <img
+              className="m-auto mx-auto"
+              src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              alt="First slide"
+            />
+            <img
+              className="m-auto mx-auto img2"
+              src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              alt="First slide"
+            />
+            <img
+              className="m-auto mx-auto"
+              src="https://images.pexels.com/photos/2539924/pexels-photo-2539924.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              alt="First slide"
+            />
+          </div>
         </Carousel.Item>
       </Carousel>
     </div>
