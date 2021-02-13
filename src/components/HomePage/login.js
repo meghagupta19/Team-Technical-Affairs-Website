87642a726552421ee6f348b9e91d4784f15aa0e0
megha <meghagupta190801@gmail.com>
2021-02-13 19:53:55 +0530

    Create login.js

---
+++ src/components/HomePage/login.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Modal,Button } from "react-bootstrap";
+import { useState } from "react";
+import Register from "./Register";
+import LoginForm from "./LoginForm"
+function Login() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <div onClick={handleShow}>Login</div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Login Now</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <LoginForm/>
+        </Modal.Body>
+        <Modal.Footer>
+          New User?
+          <Button variant="secondary" type="submit" href="/register">
+            <Register />
+          </Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default Login;
