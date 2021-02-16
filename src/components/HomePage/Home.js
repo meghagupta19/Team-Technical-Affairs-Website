2a2f80dc06a36ac47388cd6adf3aed1e35be89c9
megha <meghagupta190801@gmail.com>
2021-02-16 18:17:59 +0530

    Update Home.js

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -3,12 +3,22 @@ import "./Home.css";
 import News from "./News";
 import Events from "./Events";
 import Events2 from "./Events2";
-import { Row, Col, Container, Figure, Navbar, Nav,NavDropdown} from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Container,
+  Figure,
+  Navbar,
+  Nav,
+  NavDropdown,
+} from "react-bootstrap";
 import News2 from "./News2";
 import AboutUs from "./AboutUs";
 import Team from "./Team";
 import Login from "./login";
-import { Link, BrowserRouter } from "react-router-dom";
+import ReactDOM from 'react-dom';
+import Blogs from './Blogs'
+import { Link, BrowserRouter, BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { FaFacebook, FaLinkedin, FaTwitter, FaEnvelope } from "react-icons/fa";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -17,6 +27,15 @@ AOS.init({
   duration: 500,
   easing: "ease-in-quad",
 });
+
+ReactDOM.render(
+  <Router>
+     <Switch>
+      <Route exact path="/blogs" component={Blogs }/>
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
 
 function Home() {
   return (
@@ -35,33 +54,87 @@ function Home() {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ml-auto nav-item">
-              <Nav.Link href="#administration">Administration</Nav.Link>
-              <Nav.Link href="#events">Events</Nav.Link>
               <Nav.Link>
-                <BrowserRouter>
-                  <Link to="/" className="placed-blog">Placements</Link>
-                </BrowserRouter>
+                <a
+                  href="/"
+                  onClick={(e) => {
+                    let hero = document.getElementById("administration");
+                    e.preventDefault(); 
+                    hero && hero.scrollIntoView({ behavior: "smooth" });
+                  }}
+                >
+                  Administration
+                </a>
               </Nav.Link>
-              <Nav.Link href="#tech-team">Tech Team</Nav.Link>
+              <Nav.Link>
+                <a
+                  href="/"
+                  onClick={(e) => {
+                    let hero = document.getElementById("events");
+                    e.preventDefault(); 
+                    hero && hero.scrollIntoView({ behavior: "smooth" });
+                  }}
+                >
+                  Events
+                </a>
+              </Nav.Link>
+              <Nav.Link>
+                <Router>
+                  <Link to="/blogs" className="placed-blog">
+                    Placements
+                  </Link>
+                </Router>
+              </Nav.Link>
+              <Nav.Link>
+                <a
+                  href="/"
+                  onClick={(e) => {
+                    let hero = document.getElementById("tech-team");
+                    e.preventDefault(); 
+                    hero && hero.scrollIntoView({ behavior: "smooth" });
+                  }}
+                >
+                  Tech Team
+                </a>
+              </Nav.Link>
               <NavDropdown title="Societies" id="basic-nav-dropdown">
-                <NavDropdown.Item href="https://www.nitj.ac.in/mechanical/teams/some_society.html">Some</NavDropdown.Item>
+                <NavDropdown.Item href="https://www.nitj.ac.in/mechanical/teams/some_society.html">
+                  Some
+                </NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
                   Opengeest
                 </NavDropdown.Item>
-                <NavDropdown.Item href="https://www.nitj.ac.in/ice/spice/index.html#Events">SPice</NavDropdown.Item>
-                <NavDropdown.Item href="https://www.nitj.ac.in/ece/">SoEce</NavDropdown.Item>
+                <NavDropdown.Item href="https://www.nitj.ac.in/ice/spice/index.html#Events">
+                  SPice
+                </NavDropdown.Item>
+                <NavDropdown.Item href="https://www.nitj.ac.in/ece/">
+                  SoEce
+                </NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Ties</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">CHess</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">Soccer</NavDropdown.Item>
-                <NavDropdown.Item href="https://www.nitj.ac.in/biotech/SOBER.html">Sober</NavDropdown.Item>
+                <NavDropdown.Item href="https://www.nitj.ac.in/biotech/SOBER.html">
+                  Sober
+                </NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.4">
                   Tex Styles
                 </NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.4">IT</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.4">EE</NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link href="#contact">Contact</Nav.Link>
+              <Nav.Link>
+                <a
+                  href="/"
+                  onClick={(e) => {
+                    let hero = document.getElementById("contact");
+                    e.preventDefault(); 
+                    hero && hero.scrollIntoView({ behavior: "smooth" });
+                  }}
+                >
+                  Contact
+                </a>
+              </Nav.Link>
               <Nav.Link>
                 <Login />
               </Nav.Link>
