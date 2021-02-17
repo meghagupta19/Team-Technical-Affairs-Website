f4fa1e10305725c5fafdcd550f577c628ea20fc7
megha <meghagupta190801@gmail.com>
2021-02-17 17:38:58 +0530

    Update RegisterForm.js

--- src/components/HomePage/RegisterForm.js
+++ src/components/HomePage/RegisterForm.js
@@ -15,23 +15,44 @@ function FormExample() {
   };
 
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit} action="/register" method="post">
+    <Form
+      noValidate
+      validated={validated}
+      onSubmit={handleSubmit}
+      action="/register"
+      method="post"
+    >
       <Form.Row>
         <Form.Group as={Col} md="6" controlId="validationCustom01">
           <Form.Label>Full name</Form.Label>
-          <Form.Control required type="text" placeholder="Full name" name="name"/>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Full name"
+            name="name"
+          />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} md="6" controlId="validationCustom02">
           <Form.Label>Roll Number</Form.Label>
-          <Form.Control required type="number" placeholder="Roll Number" name="roll"/>
+          <Form.Control
+            required
+            type="number"
+            placeholder="Roll Number"
+            name="roll"
+          />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
       </Form.Row>
       <Form.Row>
         <Form.Group as={Col} md="8" controlId="validationCustom03">
           <Form.Label>Institute email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email"name="username" required />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="username"
+            required
+          />
           <Form.Control.Feedback type="invalid">
             Please provide a valid email id.
           </Form.Control.Feedback>
@@ -40,7 +61,12 @@ function FormExample() {
       <Form.Row>
         <Form.Group as={Col} md="6" controlId="validationCustom04">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password"name="password" required />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            required
+          />
           <Form.Control.Feedback type="invalid">
             Please provide a password.
           </Form.Control.Feedback>
@@ -51,7 +77,8 @@ function FormExample() {
           <Form.Label>Contact number</Form.Label>
           <Form.Control
             type="tel"
-            placeholder="Enter Contact Number" name="contact"
+            placeholder="Enter Contact Number"
+            name="contact"
             required
           />
           <Form.Control.Feedback type="invalid">
@@ -59,7 +86,9 @@ function FormExample() {
           </Form.Control.Feedback>
         </Form.Group>
       </Form.Row>
-      <Button type="submit" onClick={handleSubmit}>Register</Button>
+      <Button type="submit" onClick={handleSubmit}>
+        Register
+      </Button>
     </Form>
   );
 }
