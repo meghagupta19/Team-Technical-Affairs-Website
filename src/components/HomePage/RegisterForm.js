77aa02221e3ac4d91869d833680481704eb78643
megha <meghagupta190801@gmail.com>
2021-02-27 14:51:08 +0530

    Update RegisterForm.js

--- src/components/HomePage/RegisterForm.js
+++ src/components/HomePage/RegisterForm.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
 import { Form, Col, Button } from "react-bootstrap";
-import { useState } from "react";
-function FormExample() {
-  const [validated, setValidated] = useState(false);
 
+import axios from 'axios';
+function FormExample(props) {
+  const [validated, setValidated] = useState(false);
+  const [toggleDisplay] = useState('none');
+    const openModal = () => {
+        toggleDisplay('block');     
+    }
+  const [state , setState] = useState({
+    name:"",
+    rollno:"",
+    email : "",
+    password : "",
+    contact:""
+})
   const handleSubmit = (event) => {
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
@@ -12,7 +23,68 @@ function FormExample() {
     }
 
     setValidated(true);
+    sendDetailsToServer();
+    closeModal();
   };
+
+   const handleChange = (e) => {
+        const {id , value} = e.target   
+        setState(prevState => ({
+            ...prevState,
+            [id] : value
+        }))
+        
+    }
+    const closeModal = () => {
+      toggleDisplay('none'); 
+      
+  }
+  useEffect(() => {
+      if(props.errorMessage !== null) {
+          openModal()
+      } else {
+          closeModal()
+      }
+  });
+
+
+    const sendDetailsToServer = () => {
+      if(state.email.length && state.password.length) {
+          
+          const payload={
+            "name":state.name,
+            "rollNmuber":state.rollno,
+              "email":state.email,
+              "password":state.password,
+              "contact":state.contact
+          }
+          axios.post("http://localhost:3000/home", payload)
+              .then(function (response) {
+                  if(response.status === 200){
+                      setState(prevState => ({
+                          ...prevState,
+                          'successMessage' : 'Registration successful. Redirecting to home page..'
+                      }))
+                      
+                      redirectToHome();
+                      props.showError(null)
+                  } else{
+                      props.showError("Some error ocurred");
+                  }
+              })
+              .catch(function (error) {
+                  console.log(error);
+              });    
+      } else {
+          props.showError('Please enter valid details')    
+      }
+      
+  }
+
+  const redirectToHome = () => {
+    props.updateTitle('Home')
+    props.history.push('/home');
+}
 
   return (
     <Form
@@ -30,6 +102,9 @@ function FormExample() {
             type="text"
             placeholder="Full name"
             name="name"
+            id="name"
+            value={state.name}
+            onChange={handleChange}
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
@@ -39,7 +114,10 @@ function FormExample() {
             required
             type="number"
             placeholder="Roll Number"
+            id="rollno"
             name="roll"
+            value={state.rollno}
+            onChange={handleChange}
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
@@ -52,6 +130,9 @@ function FormExample() {
             placeholder="Enter email"
             name="username"
             required
+            id="email"
+            value={state.email}
+            onChange={handleChange}
           />
           <Form.Control.Feedback type="invalid">
             Please provide a valid email id.
@@ -65,7 +146,10 @@ function FormExample() {
             type="password"
             placeholder="Password"
             name="password"
+            id="password"
             required
+            value={state.password}
+            onChange={handleChange}
           />
           <Form.Control.Feedback type="invalid">
             Please provide a password.
@@ -73,13 +157,16 @@ function FormExample() {
         </Form.Group>
       </Form.Row>
       <Form.Row>
-        <Form.Group as={Col} md="6" controlId="validationCustom03">
+        <Form.Group as={Col} md="6" controlId="validationCustom05">
           <Form.Label>Contact number</Form.Label>
           <Form.Control
             type="tel"
             placeholder="Enter Contact Number"
             name="contact"
             required
+            id="contact"
+            value={state.contact}
+            onChange={handleChange}
           />
           <Form.Control.Feedback type="invalid">
             Please provide a valid Mobile number.
@@ -91,6 +178,10 @@ function FormExample() {
       </Button>
     </Form>
   );
+
+
+
+  
 }
 
 export default FormExample;
