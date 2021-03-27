c02e4ec5060f465a34125da8dc5db82cf64b7f6e
megha <meghagupta190801@gmail.com>
2021-03-27 22:36:48 +0530

    Update Blogs.js

--- src/components/HomePage/Blogs.js
+++ src/components/HomePage/Blogs.js
@@ -3,27 +3,35 @@ import {
   Navbar,
   Nav,
   NavDropdown,
-  ListGroup,
   NavItem,
-  ListGroupItem,
   Card,
   Container,
   Row,
   Col,
 } from "react-bootstrap";
+import { Modal, Button, Collapse } from "react-bootstrap";
 import { isAuthenticated, signout } from "../../auth/authHelper";
 import Login from "./login";
 import { Link } from "react-router-dom";
 import Contact from "./Contact";
 import axios from "axios";
 import { API_URL } from "../../config";
+import { CardHeader } from "@material-ui/core";
 
 function Blogs() {
   const [setLogin] = useState(false);
+  const [show, setShow] = useState(false);
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const [readMore,setReadMore]=useState(false);
+  const linkName=readMore?'Read Less << ':'Read More >> '
   const [state, setState] = useState({
     name: "",
     username: "",
+    title: "",
     exp: "",
+    state:"",
     blogs: [],
   });
   useEffect(() => {
@@ -38,12 +46,14 @@ function Blogs() {
     const { name, value } = e.target;
     setState({ ...state, [name]: value });
   }
-  function handleOnSubmit(e) {
+  function handleOnSave(e) {
     e.preventDefault();
     const data = {
       name: state.name,
       username: state.username,
+      title: state.title,
       exp: state.exp,
+      tags: state.tags,
     };
 
     axios
@@ -56,10 +66,13 @@ function Blogs() {
     setState({
       name: "",
       username: "",
+      title: "",
       exp: "",
+      tags:"",
       blogs: [...state.blogs, data],
     });
   }
+
   function blogs() {
     return state.blogs.map((blog) => {
       return (
@@ -67,10 +80,31 @@ function Blogs() {
           <Card>
             <Card.Body>
               <Card.Title>{blog.name}</Card.Title>
+              <h5>{blog.title}</h5>
+              <Card.Text>{blog.exp}</Card.Text>
+            </Card.Body>
+              <div className="blog-button-div">
+            <Button className="blog-button">Edit</Button>
+            <button className="blog-button" type="submit">
+              Send
+            </button>
+            </div>
+          </Card>
+        </Col>
+      );
+    });
+  }
+  function blogs2() {
+    return state.blogs.map((blog) => {
+      return (
+        <Col md={12} className="blog-col2">
+          <Card>
+            <Card.Body>
+              <Card.Title>{blog.name}</Card.Title>
+              <h5>{blog.title}</h5>
               <Card.Text>{blog.exp}</Card.Text>
             </Card.Body>
           </Card>
-          <hr></hr>
         </Col>
       );
     });
@@ -78,7 +112,7 @@ function Blogs() {
   const loginDetails = () => {
     if (isAuthenticated()) {
       return (
-        <div>
+        <div className="link-color">
           <NavItem>Welcome {localStorage.getItem("name")}!</NavItem>
           <Nav.Link
             onClick={() => {
@@ -99,7 +133,7 @@ function Blogs() {
   };
   return (
     <div className="body2">
-      <div className="top2">
+      <div className="top2 navbar-blog">
         <Navbar
           collapseOnSelect
           expand="lg"
@@ -114,12 +148,16 @@ function Blogs() {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ml-auto nav-item nav-blog">
               <Nav.Link>
-                <Link to="/" className="placed-blog">
+                <Link to="/" className="placed-blog link-color">
                   Home
                 </Link>
               </Nav.Link>
 
-              <NavDropdown title="Societies" id="basic-nav-dropdown">
+              <NavDropdown
+                title="Societies"
+                id="basic-nav-dropdown"
+                className="link-color"
+              >
                 <NavDropdown.Item href="https://www.nitj.ac.in/mechanical/teams/some_society.html">
                   Some
                 </NavDropdown.Item>
@@ -153,6 +191,7 @@ function Blogs() {
                     e.preventDefault();
                     hero && hero.scrollIntoView({ behavior: "smooth" });
                   }}
+                  className="link-color"
                 >
                   Contact
                 </a>
@@ -163,54 +202,166 @@ function Blogs() {
         </Navbar>
       </div>
       <div className="placements">
-        <Container>
-          <Row>
-            <Col md={8}>
-              <Container className="blog-list">
-                <Row className="blog-row">{blogs()}</Row>
-              </Container>
+        <Container className="blog-list">
+          <Row className="blog-row">
+            <Col md={6} className="blog-col">
+              <Card>
+                <Card.Body>
+                  <Card.Title>Name</Card.Title>
+                  <h5>Blog Title !!!</h5>
+                  <Card.Text>
+                    Content.............
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              </Col>
+              <Col md={6} className="blog-col">
+              <Card>
+                <Card.Body>
+                  <Card.Title>Name</Card.Title>
+                  <h5>Blog Title !!!</h5>
+                  <Card.Text>
+                    Content.............                   
+                  </Card.Text>
+                </Card.Body>
+              </Card>
             </Col>
-            <Col md={4}></Col>
           </Row>
-          <Row>
-            <form className="blog-form">
-              <h3>Share Your Experience</h3>
-              <input
-                className="blog-name"
-                placeholder="Name"
-                type="text"
-                name="name"
-                onChange={handleOnChange}
-                value={state.name}
-              ></input>
-              <input
-                className="user"
-                placeholder="Email Id"
-                type="email"
-                name="username"
-                onChange={handleOnChange}
-                value={state.username}
-              />
-
-              <textarea
-                className="message"
-                placeholder="Type a message here"
-                name="exp"
-                rows="5"
-                columns="15"
-                onChange={handleOnChange}
-                value={state.exp}
-              ></textarea>
-              <button
-                className="blog-button"
-                type="submit"
-                onClick={handleOnSubmit}
-              >
-                Send
-              </button>
-            </form>
+          <Row className="blog-row">
+            <Col md={6} className="blog-col">
+              <Card>
+                <Card.Body>
+                  <Card.Title>Name</Card.Title>
+                  <h5>Blog Title !!!</h5>
+                  <Card.Text>
+                    Content.............
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              </Col>
+              <Col md={6} className="blog-col">
+              <Card>
+                <Card.Body>
+                  <Card.Title>Name</Card.Title>
+                  <h5>Blog Title !!!</h5>
+                  <Card.Text>
+                    Content.............                   
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
           </Row>
         </Container>
+        <h3
+          className="blog-form-head"
+          onClick={() => setOpen(!open)}
+          aria-controls="example-collapse-text"
+          aria-expanded={open}
+        >
+          Write Your Experience
+        </h3>
+        <h5 className="blog-form-head">
+        Your small contributions can make someone learn easier and better
+        </h5>
+        <Collapse in={open}>
+          <Container>
+            <Row className="form-send">
+              <Col>
+                <form className="blog-form">
+                <Col md={6} className="blog-form-col">
+                  <label>Title</label>
+                  <input
+                    className="title-blog"
+                    name="title"
+                    onChange={handleOnChange}
+                    value={state.title}
+                  ></input>
+                  <label>Content</label>
+                  <textarea
+                    className="message"
+                    placeholder="Type your content here"
+                    name="exp"
+                    rows="10"
+                    columns="25"
+                    onChange={handleOnChange}
+                    value={state.exp}
+                  ></textarea>
+                  
+                    <button className="blog-button" type="submit">
+                      Send
+                    </button>                  
+                    </Col>
+                    
+                <Col md={6} className="blog-form-col">
+                <label>Name</label>
+                  <input
+                    className="blog-name"
+                    placeholder="Full name"
+                    type="text"
+                    name="name"
+                    onChange={handleOnChange}
+                    value={state.name}
+                  ></input>
+                  <label>Email Id</label>
+                  <input
+                    className="user"
+                    placeholder="Official Email Id"
+                    type="email"
+                    name="username"
+                    onChange={handleOnChange}
+                    value={state.username}
+                  />
+                  <input className="tags" name="tags" type="text" value="" data-role="tagsinput" id="tags" class="form-control"
+                  onChange={handleOnChange}
+                  value={state.tags}/>
+                  <div className="blog-button-div">
+                    <button
+                      className="blog-button"
+                      type="submit"
+                      onClick={handleOnSave}
+                    >
+                      Save
+                    </button>
+                    <button
+                      className="blog-button"
+                      onClick={handleShow}
+                    >
+                      Preview
+                    </button>
+                    <Modal
+                      show={show}
+                      onHide={handleClose}
+                      size="lg"
+                      aria-labelledby="contained-modal-title-vcenter"
+                      centered
+                      className="modal-blog"
+                    >
+                      <Modal.Header closeButton>
+                      </Modal.Header>
+                      <Modal.Body>
+                        <Container className="blog-list">
+                          <Row className="blog-row">{blogs2()}</Row>
+                        </Container>
+                      </Modal.Body>
+                    </Modal>
+                  </div>
+              
+                  </Col>
+                  
+                </form>
+              </Col>
+              
+              
+            </Row>
+            <Row>
+              <Col>
+                <Container className="blog-list">
+                  <Row className="blog-row">{blogs()}</Row>
+                </Container>
+              </Col>
+            </Row>
+          </Container>
+        </Collapse>
       </div>
       <div className="bottom">
         <Contact />
