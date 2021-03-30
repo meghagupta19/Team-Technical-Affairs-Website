b5454089c892fe55745d2d237d6ae0247b0935f0
megha <meghagupta190801@gmail.com>
2021-03-30 20:04:46 +0530

    Update blog.js

--- Server/Models/blog.js
+++ Server/Models/blog.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const blogSchema = new mongoose.Schema({
+    name: String,
     username: String,
-    exp : String
+    exp : String,
+    title: String
 })
 
 module.exports = mongoose.model('Blog',blogSchema);
